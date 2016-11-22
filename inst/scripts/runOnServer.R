7c2317e9e21303a3ba647814f1ff33dc66b4ac81
sorhawell <sorhawell@gmail.com>
2016-11-22 12:22:14 +0100

    rework order of script, minor bugs, interface to walltime,proc and nodes, flag out tickets by class ticket

--- inst/scripts/runOnServer.R
+++ inst/scripts/runOnServer.R
@@ -10,14 +10,13 @@
 
 
 #This script has step 1 to 9
-
 print("Printing from R backend on server...")
 print(Sys.time())
 
 ##1 set work directory
 #get Tempdir.backend file path
-args = commandArgs(trailingOnly=TRUE)
-Tempdir.backend = args[1]
+args = commandArgs(trailingOnly=TRUE) #get wd from command arg
+Tempdir.backend = args[1] #first argument is wd
 print(args)
 # cat("set backend work directory to: ",Tempdir.backend,"\n")
 setwd(Tempdir.backend)
@@ -28,112 +27,145 @@ print("Loading exported variables ...")
 load(file="Tempexp.rda")
 
 ##2b If this script is set to run async, it will submit itself to qsub
-#To avoid infinite recursive calling, this script will call it self with the async_stop arg
-#Therefore, when this script is run through qsub it will execute and return, not submit itself again
+#To avoid infinite recursive calling, this script will call itself with the async_stop arg
+#Therefore, when this script is run through qsub it will execute and return,
+#... and not submit itself again
 
+#If second commandArg is "async_stop" unsubmit
 if(!is.null(args[2]) && !is.na(args[2]) && args[2][[1]] == "async_stop") {
   export$async = FALSE
-  print(export$async)
-} #turn of submitting itself to qsub
+} #turn off submitting itself to qsub
 
 ##3 Before loading BatchJobs packages... some config files must be placed in user root
+#Place one config file in user root, BatchJobs package will load this config file when loaded
+#write qsub file
+#3a
+#files to submit this script to qsub
+if(export$async) {
+  writeLines(text = paste0(
+    "#!/bin/sh
 
-#place one config file in user root, BatchJobs package will load this config file when loaded
-writeLines(text =
-             "# Torque/PBS cluster
-           cluster.functions <- makeClusterFunctionsTorque('./fastRditijuu_qsub.tmpl')",
-con = "~/.BatchJobs.R"
-)
+    #PBS -l nodes=",export$qsub.nodes,":ppn=",export$qsub.proc,"
+    #PBS -l walltime=",export$qsub.walltime,"
 
-#place one config file in tmp folder, BatchJobs pacakge will use this config file to form qsub's
+    echo $CPUTYPE
+
+    #PBS -N <%= job.name %>
+    ## merge standard error and output
+    #PBS -j oe
+    ## direct streams to our logfile
+    #PBS -o <%= log.file %>
+
+
+    ## Run R:
+    ##runserver script will submit itself to qsub",
+    "\n",
+    "cd ", Tempdir.backend,
+    "\n",
+    "R CMD BATCH --no-save --no-restore '--args ",Tempdir.backend ," async_stop' runOnServer.R /dev/stdout",
+
+    "\n \n"),con = "./fastRditijuu_qsub_async.sh")
+} else {
+  #write config files for BatchJob package
+  if("BatchJobs" %in% export$packages) {
+  print("saving file to ")
   writeLines(text =
+               "# Torque/PBS cluster
+             cluster.functions <- makeClusterFunctionsTorque('./fastRditijuu_qsub_BatchJobs.sh')",
+             con = "~/.BatchJobs.R"
+  )
+
+  #place one config file in tmp folder, BatchJobs pacakge will use this config file to form qsub's
+  writeLines(paste0(text =
 "#!/bin/sh
-
-#PBS -l nodes=1:ppn=1
-#PBS -l walltime=00:08:00
-
+#PBS -l nodes=",export$qsub.nodes,":ppn=",export$qsub.proc,"
+#PBS -l walltime=",export$qsub.walltime,"
 echo $CPUTYPE
-
 #PBS -N <%= job.name %>
 ## merge standard error and output
 #PBS -j oe
 ## direct streams to our logfile
 #PBS -o <%= log.file %>
-
-
 ## Run R:
 ## we merge R output with stdout from PBS, which gets then logged via -o option
 R CMD BATCH --no-save --no-restore '<%= rscript %>' /dev/stdout
-",
-             con = "./fastRditijuu_qsub.tmpl"
-)
+"),con = './fastRditijuu_qsub_BatchJobs.sh'
+  )
+  }
+}
 
-##4 Handle packages
-print("Check, install and load packages...")
-list.of.packages <- export$packages
-#from http://stackoverflow.com/questions/4090169
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages,repos="http://cran.us.r-project.org")
-for(aPackage in list.of.packages) require(package=aPackage,character.only = T)
-print("following packages loaded on master")
-print(search())
+##4 Handle packages, skip if async
+if(!export$async) {
+  print("Check, install and load packages...")
+  list.of.packages <- export$packages
+  #from http://stackoverflow.com/questions/4090169
+  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+  if(length(new.packages)) install.packages(new.packages,repos="http://cran.us.r-project.org")
+  for(aPackage in list.of.packages) require(package=aPackage,character.only = T)
+  print("following packages loaded on master")
+  print(search())
+}
 
 
 ##4b - Define a wrapper function doBatchJob only for BatchJobs
 # preparation for starting slave nodes, only when using BatchJobs package
-if("package:BatchJobs" %in% search() && export$async==FALSE) {
-  #source doBatchJob function on server side
-  #this wrapper is handling job-arrays (to split a list of jobs in separate job lists)
-  #...and global variables (loads an global variable state)
-  #...and combines individual slave node results into list of all results
-  doBatchJob = function(
-    X,FUN,packages=c(),max.nodes=24,globalVar=list(),...) {
-    #BatchJobs package only needs to be loaded on master node, not on slaves
+if(!export$async) {
+  if("package:BatchJobs" %in% search() && export$async==FALSE) {
+    #source doBatchJob function on server side
+    #this wrapper is handling job-arrays (to split a list of jobs in separate job lists)
+    #...and global variables (loads an global variable state)
+    #...and combines individual slave node results into list of all results
+    doBatchJob = function(X,FUN,packages=c(),max.nodes=24,globalVar=list(),...) {
+      #BatchJobs package only needs to be loaded on master node, not on slaves
 
-    #split jobs into one job-list for each node
-    cluster.nodes = min(length(X),max.nodes) #no more nodes required than jobs
-    jobArrays = suppressWarnings(split(X,1:cluster.nodes))
-    splitKey  = unlist(suppressWarnings(split(1:length(X),1:cluster.nodes)),use.names = FALSE)
-    invSplitKey = match(1:length(X),splitKey)
-    #suppress warning, when nodes get ueven amount of jobs.
+      #split jobs into one job-list for each node
+      cluster.nodes = min(length(X),max.nodes) #no more nodes required than jobs
+      jobArrays = suppressWarnings(split(X,1:cluster.nodes))
+      splitKey  = unlist(suppressWarnings(split(1:length(X),1:cluster.nodes)),use.names = FALSE)
+      invSplitKey = match(1:length(X),splitKey)
+      #suppress warning, when nodes get ueven amount of jobs.
 
-    #Meeseeks box(Rick & Morty reference) executer of job-lists
-    wrapB = function(X,FUN,...) {
-      print("I'm Mr Meeseeks(a torque/PBS cluster slave), look at me!!!")
-      cat("Oh geee, my work directory is",getwd(),"\n")
+      #Meeseeks box(Rick & Morty reference) executer of job-lists
+      wrapMeeseeks = function(X,FUN,...) {
+        print("I'm Mr Meeseeks (a torque/PBS cluster slave), look at me!!!")
+        cat("Oh geee, my work directory is",getwd(),"\n")
 
-      #load attach global vars on slave machine
-      if(file.exists('globalVar.rda')) {
-        print("global variables detected, loading...")
-        load('globalVar.rda')
-        attach(globalVar)
+        #load attach global vars on slave machine
+        if(file.exists('globalVar.rda')) {
+          print("global variables detected, loading...")
+          load('globalVar.rda')
+          attach(globalVar)
+        }
+
+        #run array of jobs on this slave
+        print("Master: Mr Meeseeks, please iterate this job-list with lapply")
+        print("Mr Meeseeks: 'Sure can do!!'")
+        out = lapply(X,function(X) do.call(eval(FUN),list(X,...)),...)
+        print("Mr Meeseeks: Job completed, pooofff!!")
+        return(out)
       }
 
-      #run array of jobs on this slave
-      print("Master: Mr Meeseeks, please iterate this job-list with lapply")
-      print("Mr Meeseeks: 'Sure can do!!'")
-      out = lapply(X,function(X) do.call(eval(FUN),list(X,...)),...)
-      print("Mr Meeseeks: Job completed, pooofff!!")
+      #cluster.functions = makeClusterFunctionsTorque("./fastRditijuu_qsub.sh") #this file is created below
+
+      #ensure empty package list are character(0) and remove BatchJobs package, as it is not needed on slaves
+      packages = if(length(packages)) packages[packages!="BatchJobs"] else character(0L)
+      reg <- makeRegistry(id="testBatchJobs",packages=if(length(packages)) packages else character(0L))
+      save(reg,file="testBatchJobs-files/reg.rda")
+      batchMap(reg,fun=wrapMeeseeks,X=jobArrays,use.names=T,more.args = c(list(FUN=FUN,...)))
+      submitJobs(reg)
+      print("jobs have been submitted")
+      waitForJobs(reg)
+      print("All jobs has finished")
+      out = unlist(loadResults(reg,1:cluster.nodes),recursive = FALSE)
+      out = out[invSplitKey] #re-order jobs by inverted splitKey
+      removeRegistry(reg,ask="no")
       return(out)
     }
-
-    cluster.functions = makeClusterFunctionsTorque("./fastRditijuu_qsub.tmpl") #this file is created below
-    reg <- makeRegistry(id="testBatchJobs",packages=if(length(packages)) packages else character(0L))
-    save(reg,file="testBatchJobs-files/reg.rda")
-    batchMap(reg,fun=wrapB,X=jobArrays,use.names=T,more.args = c(list(FUN=FUN,...)))
-    submitJobs(reg)
-    waitForJobs(reg)
-    out = unlist(loadResults(reg,1:cluster.nodes),recursive = FALSE)
-    out = out[invSplitKey] #re-order jobs by inverted splitKey
-    removeRegistry(reg,ask="no")
-    return(out)
   }
 }
 
-
-
 ##6 handling global variables
-if(length(export$globalVar)) {
+if(!export$async && length(export$globalVar)) { #if any variables
   if("package:BatchJobs" %in% search()) {
     #if job in executed with BatchJobs, save globalVarible to separate file
     cat("Export these variables to slave nodes",names(export$globalVar) ,"\n")
@@ -146,49 +178,18 @@ if(length(export$globalVar)) {
     print("following global variables are attached to Master node environment:")
     print(names(export$globalVar))
   }
-} else {
-  print("no global variables exported to server side")
 }
 
 ##7 - excution on master node
 if(!export$async) {
   print("calling the function")
   out = do.call(eval(export$what),export$arg,quote=T)
-
-
-
 } else {
-  #write qsub file
-  writeLines(text = paste0(
-               "#!/bin/sh
-
-#PBS -l nodes=1:ppn=1
-#PBS -l walltime=00:08:00
-
-echo $CPUTYPE
-
-#PBS -N <%= job.name %>
-## merge standard error and output
-#PBS -j oe
-## direct streams to our logfile
-#PBS -o <%= log.file %>
-
-
-## Run R:
-##runserver script will submit itself to qsub",
-"\n",
-"cd ", Tempdir.backend,
-"\n",
-"R CMD BATCH --no-save --no-restore '--args ",Tempdir.backend ," async_stop' runOnServer.R /dev/stdout",
-
-"\n \n"),con = "./fastRditijuu_qsub_async.sh")
-
-##runserver script will submit itself to qsub
-jobNumber = system("qsub fastRditijuu_qsub_async.sh",intern=TRUE)
-print("yep we got to here")
-
-out = list(backend.tmp = getwd(),jobNumber=jobNumber)
-
+  ##runserver script will submit itself to qsub
+  jobNumber = system("qsub fastRditijuu_qsub_async.sh",intern=TRUE)
+  print("yep we got to here")
+  out = list(backend.tmp = getwd(),jobNumber = jobNumber)
+  class(out) = "ticket"
 }
 
 ##8 - saving
@@ -198,9 +199,7 @@ saveRDS(out,file="Tempout.rda")
 ##9 say goodbye
 print("Hello Master, this is HAL 9000. Work completed, returning to local.")
 print(Sys.time())
+
 if(export$async==FALSE)
-  writeLines("Hello Master, this is HAL 9000. Work completed, returning to local.",
-             con="job_completed")
-
-
-
+writeLines("Hello Master, this is HAL 9000. Work completed, returning to local.",
+             con="job_completed.txt")
